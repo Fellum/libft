d7d774fd44a6077b48fb1f34f4356124e3c9d6d9
Jasper Leann <jleann@oa-j2.21-school.ru>
2019-04-17 20:15:27 +0300

    added limits.h fix

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: jleann <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/02 20:45:49 by jleann            #+#    #+#             */
-/*   Updated: 2019/04/02 20:45:50 by jleann           ###   ########.fr       */
+/*   Updated: 2019/04/17 20:07:53 by jleann           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,15 @@
 # include <unistd.h>
 # include <string.h>
 
-# define LONG_MAX	9223372036854775807
-# define LONG_MIN	(-LONG_MAX -1L)
-# define ULONG_MAX	(LONG_MAX *2UL+1UL)
+# ifndef LONG_MAX
+#  define LONG_MAX	9223372036854775807
+# endif
+# ifndef LONG_MIN
+#  define LONG_MIN	(-LONG_MAX -1L)
+# endif
+# ifndef ULONG_MAX
+#  define ULONG_MAX	(LONG_MAX *2UL+1UL)
+# endif
 
 typedef unsigned char	t_byte;
 typedef	struct			s_list
