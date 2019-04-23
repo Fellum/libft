948e340f52c7c34de3175a698a6dadb569973bf9
Fellum <fkekerman@gmail.com>
2019-04-23 12:42:04 +0300

    fixed lsterror

--- srcs/ft_lstfind.c
+++ srcs/ft_lstfind.c
@@ -6,20 +6,19 @@
 /*   By: jleann <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/05 18:50:39 by jleann            #+#    #+#             */
-/*   Updated: 2019/04/05 18:50:40 by jleann           ###   ########.fr       */
+/*   Updated: 2019/04/23 12:41:26 by jleann           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_lstfind(t_list *alst, void *content, int (*f)(void *, void *,
-		size_t size)
+void	*ft_lstfind(t_list *alst, void *content, int (*f)(void *, void *))
 {
 	if (!alst || !f)
 		return (NULL);
 	while (alst)
 	{
-		if (f(alst->content, content, size) == 0)
+		if (f(alst->content, content) == 0)
 			return (alst);
 		alst = alst->next;
 	}
